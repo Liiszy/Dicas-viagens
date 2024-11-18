9f2ee823fa2c3c2c409148636ca65eea90cc985c
Lais Alonso <167369632+Liiszy@users.noreply.github.com>
2024-11-18 17:47:41 -0300

    comitt13

--- js/turismo.js
+++ js/turismo.js
@@ -1,21 +1,36 @@
-// Exibe o nome do último usuário logado
-const lastLoggedUser = localStorage.getItem('lastLoggedUser');
-
-// Atualiza o nome do usuário e controla a visibilidade do botão de logout
-if (lastLoggedUser) {
-    document.getElementById('name').textContent = lastLoggedUser;
-    document.getElementById('logoutBtn').style.display = 'inline-block'; // Mostrar botão
-} else {
-    document.getElementById('name').textContent;
-    document.getElementById('logoutBtn').style.display = 'none'; // Ocultar botão
-}
-
-// Função de logout
-document.getElementById('logoutBtn').addEventListener('click', function() {
-    // Remove o usuário logado do localStorage
-    localStorage.removeItem('lastLoggedUser');
-   
-
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio do formulário tradicional
+  
+    // Pega os valores dos campos de entrada
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+  
+    // Verifica se as senhas são iguais
+    if (password !== confirmPassword) {
+        
+        return; // Para o processo aqui se as senhas não forem iguais
+    }
+  
+    // Obtém os usuários cadastrados do localStorage ou cria um array vazio se não houver
+    let users = JSON.parse(localStorage.getItem('users')) || [];
+  
+    // Verifica se o email de usuário já existe
+    const userExists = users.some(user => user.email === email);
+    if (userExists) {
+       
+        return;
+    }
+  
+    // Adiciona o novo usuário ao array
+    users.push({ email: email, password: password, username: username});
+  
+    // Armazena o array de usuários de volta no localStorage
+    localStorage.setItem('users', JSON.stringify(users));
+  
+   ;
     // Redireciona para a página de login
-    window.location.href = 'index.html'; // Redireciona para a página de login
-});
+    window.location.href = 'login.html';
+  });
+  
