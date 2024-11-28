aeb077e9b32557f939aa5063816f32a505a38922
Lais Alonso <167369632+Liiszy@users.noreply.github.com>
2024-11-28 16:44:47 -0300

    final

--- js/index.js
+++ js/index.js
@@ -1,21 +1,37 @@
 // Exibe o nome do último usuário logado
 const lastLoggedUser = localStorage.getItem('lastLoggedUser');
 
+
 // Atualiza o nome do usuário e controla a visibilidade do botão de logout
 if (lastLoggedUser) {
     document.getElementById('name').textContent = lastLoggedUser;
     document.getElementById('logoutBtn').style.display = 'inline-block'; // Mostrar botão
+
 } else {
     document.getElementById('name').textContent;
     document.getElementById('logoutBtn').style.display = 'none'; // Ocultar botão
+
+
 }
 
 // Função de logout
-document.getElementById('logoutBtn').addEventListener('click', function() {
+document.getElementById('logoutBtn').addEventListener('click', function () {
     // Remove o usuário logado do localStorage
     localStorage.removeItem('lastLoggedUser');
-   
+    localStorage.removeItem('lastAccess')
+
 
     // Redireciona para a página de login
     window.location.href = 'index.html'; // Redireciona para a página de login
 });
+
+    const login=document.querySelector(".login-icon")
+    if(lastLoggedUser&&lastLoggedUser!==""){
+        login.classList.add("logged")
+        console.log("logado")
+    }else{
+        login.classList.remove("logged")
+        console.log("empty")
+    } 
+
+    
--- js/login.js
+++ js/login.js
@@ -10,15 +10,15 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
   
     // Verifica se as credenciais correspondem a algum usuário cadastrado
     const validUser = users.find(user => user.email === email && user.password === password);
-  
+  const erro= document.querySelector(".erro")
     if (validUser) {
         // Armazena o último usuário logado
         localStorage.setItem('lastLoggedUser',validUser.username);
         localStorage.setItem('lastAccess',validUser.access)
        
         // Redireciona para a página comunidade.html
-        window.location.href = 'index.html';
+       event.target.submit();
     } else {
-       
+       erro.innerHTML="*Usuário ou senha inválido!"
     }
   });   
